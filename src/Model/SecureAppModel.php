852bc80c1c036d3870f3ef51307ad2bfe8c77895
Chief Adofoh <chiefadofoh1@gmail.com>
2023-12-05 17:16:07 +0000

    implementing json validation

--- src/Model/SecureAppModel.php
+++ src/Model/SecureAppModel.php
@@ -10,53 +10,52 @@ class SecureAppModel
         $this->db = $db;
     }
     public function storeData(
-           $dateInput1,
-           $dateInput2,
-           $phoneInput,
-           $jsonInput,
-           $emailInput,
-           $passwordInput,
-           $postcodeInput,
-           $creditCardInput,
-           $ipInput,
-           $additionalInput
-       )
-       {
-           // Validate inputs
-           if (
-               !$this->isValidDate($dateInput1) || !$this->isValidDate($dateInput2) ||
-               !$this->isValidPhoneNumber($phoneInput) || !$this->isValidJson($jsonInput) ||
-               !$this->isValidEmail($emailInput) || !$this->isValidPassword($passwordInput) ||
-               !$this->isValidAddress($postcodeInput) || !$this->isValidCreditCard($creditCardInput) ||
-               !$this->isValidIpAddress($ipInput)
-           ) {
-               // One or more inputs are invalid
-               return false;
-           }
-   
-           // Input validation passed, store data in the database
-           try {
-               $stmt = $this->db->prepare(
-                   'INSERT INTO client_data 
+        $dateInput1,
+        $dateInput2,
+        $phoneInput,
+        $jsonInput,
+        $emailInput,
+        $passwordInput,
+        $postcodeInput,
+        $creditCardInput,
+        $ipInput,
+        $additionalInput
+    ) {
+        // Validate inputs
+        if (
+            !$this->isValidDate($dateInput1) || !$this->isValidDate($dateInput2) ||
+            !$this->isValidPhoneNumber($phoneInput) || !$this->isValidJson($jsonInput) ||
+            !$this->isValidEmail($emailInput) || !$this->isValidPassword($passwordInput) ||
+            !$this->isValidPostcode($postcodeInput) || !$this->isValidCreditCard($creditCardInput) ||
+            !$this->isValidIpAddress($ipInput)
+        ) {
+            // One or more inputs are invalid
+            return false;
+        }
+
+        // Input validation passed, store data in the database
+        try {
+            $stmt = $this->db->prepare(
+                'INSERT INTO client_data 
                    (dateInput1, dateInput2, phoneInput, jsonInput, emailInput, passwordInput, 
                    postcodeInput, creditCardInput, ipInput, additionalInput) 
                    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'
-               );
-   
-               $stmt->execute([
-                   $dateInput1, $dateInput2, $phoneInput, $jsonInput, $emailInput, $passwordInput,
-                   $postcodeInput, $creditCardInput, $ipInput, $additionalInput
-               ]);
-   
-               return true;
-           } catch (PDOException $e) {
-               // Log or handle the database error as needed
-               return false;
-           }
-       }
-   
-       private function isValidDate($date)
-       {
+            );
+
+            $stmt->execute([
+                $dateInput1, $dateInput2, $phoneInput, $jsonInput, $emailInput, $passwordInput,
+                $postcodeInput, $creditCardInput, $ipInput, $additionalInput
+            ]);
+
+            return true;
+        } catch (PDOException $e) {
+            // Log or handle the database error as needed
+            return false;
+        }
+    }
+
+    private function isValidDate($date)
+    {
         // Validate against 'yyyy-mm-dd' format
         $dateTime1 = DateTime::createFromFormat('Y-m-d', $date);
 
@@ -65,56 +64,75 @@ class SecureAppModel
 
         return ($dateTime1 && $dateTime1->format('Y-m-d') === $date) ||
             ($dateTime2 && $dateTime2->format('d/m/Y') === $date);
-       }
-   
-       private function isValidPhoneNumber($phoneNumber)
-       {
-           // Implement phone number validation logic, return true if valid, false otherwise
-           // Example: Use regular expressions to validate the phone number format
-           return true;
-       }
-   
-       private function isValidJson($json)
-       {
-           // Implement JSON validation logic, return true if valid, false otherwise
-           // Example: Use json_decode() and check for errors
-           return true;
-       }
-   
-       private function isValidEmail($email)
-       {
-           // Implement email validation logic, return true if valid, false otherwise
-           // Example: Use filter_var() with FILTER_VALIDATE_EMAIL
-           return true;
-       }
-   
-       private function isValidPassword($password)
-       {
-           // Implement password validation logic, return true if valid, false otherwise
-           // Example: Check for password strength requirements
-           return true;
-       }
-   
-       private function isValidPostcode($postcode)
-       {
-           // Implement address validation logic, return true if valid, false otherwise
-           // Example: Check for address format or use external APIs for validation
-           return true;
-       }
-   
-       private function isValidCreditCard($creditCard)
-       {
-           // Implement credit card validation logic, return true if valid, false otherwise
-           // Example: Use algorithms like Luhn's algorithm to check credit card numbers
-           return true;
-       }
-   
-       private function isValidIpAddress($ipAddress)
-       {
-           // Implement IP address validation logic, return true if valid, false otherwise
-           // Example: Use filter_var() with FILTER_VALIDATE_IP
-           return true;
-       }
-   }
-   ?>
+    }
 
+    private function isValidPhoneNumber($phoneNumber)
+    {
+        // Implement phone number validation logic, return true if valid, false otherwise
+        // Example: Use regular expressions to validate the phone number format
+        return true;
+    }
+
+    private function isValidJson($json)
+    {
+        // URL of the JSONLint API
+        $jsonLintApiUrl = 'https://jsonlint.com/api';
+
+        // Set up the POST request
+        $postData = [
+            'json' => $json,
+        ];
+
+        $options = [
+            'http' => [
+                'header'  => 'Content-type: application/x-www-form-urlencoded',
+                'method'  => 'POST',
+                'content' => http_build_query($postData),
+            ],
+        ];
+
+        $context  = stream_context_create($options);
+        $response = file_get_contents($jsonLintApiUrl, false, $context);
+
+        // Decode the JSON response
+        $result = json_decode($response);
+
+        // Check if the JSON is valid
+        return isset($result->error) ? false : true;
+    }
+
+    private function isValidEmail($email)
+    {
+        // Implement email validation logic, return true if valid, false otherwise
+        // Example: Use filter_var() with FILTER_VALIDATE_EMAIL
+        return true;
+    }
+
+    private function isValidPassword($password)
+    {
+        // Implement password validation logic, return true if valid, false otherwise
+        // Example: Check for password strength requirements
+        return true;
+    }
+
+    private function isValidPostcode($postcode)
+    {
+        // Implement address validation logic, return true if valid, false otherwise
+        // Example: Check for address format or use external APIs for validation
+        return true;
+    }
+
+    private function isValidCreditCard($creditCard)
+    {
+        // Implement credit card validation logic, return true if valid, false otherwise
+        // Example: Use algorithms like Luhn's algorithm to check credit card numbers
+        return true;
+    }
+
+    private function isValidIpAddress($ipAddress)
+    {
+        // Implement IP address validation logic, return true if valid, false otherwise
+        // Example: Use filter_var() with FILTER_VALIDATE_IP
+        return true;
+    }
+}
